9762a2c62467ddce7406c5ab9f2016cc6056e650
chobocho <chobocho99@gmail.com>
2026-01-30 19:57:13 +0900

    Save notepad changes on window close if unsaved adjustments detected

--- ssMemoWeb/src/main.js
+++ ssMemoWeb/src/main.js
@@ -5,11 +5,19 @@
 import { NoteSearchUI } from './note-search.js';
 import { Notepad } from './notepad.js';
 import { AppAPI } from './app-api.js';
+import { state } from './state.js';
 
 async function initApp() {
     await AppAPI.init();
     NoteSearchUI.setup();
     Notepad.open();
 }
+
+// Save notepad when closing the window if there are changes
+window.addEventListener('beforeunload', async (e) => {
+    if (state.notepad.isDirty) {
+        await Notepad.save();
+    }
+});
 
 window.addEventListener('load', initApp);
